25e978f133ee81e3f23840665c53cbe698c29103
ksh9241 <ksh9241@naver.com>
2022-02-06 20:25:58 +0900

    토이프로젝트

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -5,6 +5,27 @@ document.addEventListener("DOMContentLoaded", function() {
 function clickLoginBt () {
 	let loginBt = document.querySelector(".login_btn");
 	loginBt.addEventListener("click", function () {
-		alert("클릭됨.");
+		
+		let loginForm = document.querySelector("#loginForm");
+		if (validation(loginForm)) {
+			loginForm.submit();		
+		};
 	})
+}
+
+function validation (loginForm) {
+	let id = loginForm.querySelector(".input_user").value;
+	let pw = loginForm.querySelector(".input_pass").value;
+	
+	if (id.trim() == '' || id == null) {
+		alert("아이디를 입력해주세요.");
+		loginForm.querySelector(".input_user").focus();
+		return;
+	} else if (pw.trim() == '' || pw == null) {
+		alert("패스워드를 입력해주세요.");
+		loginForm.querySelector(".input_pass").focus();
+		return;
+	}
+	
+	return true;
 }
